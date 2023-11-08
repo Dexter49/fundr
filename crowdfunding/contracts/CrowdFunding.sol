8ccb7c3e3ec48a9c0cc03c428050ee4513a72d3b
Dexter49 <dexterousguru49@yahoo.com>
2023-11-08 14:12:56 +0100

    Finished with the `donateToCampaign` function in `CrowdFunding.sol`... Moving to the next one

--- crowdfunding/contracts/CrowdFunding.sol
+++ crowdfunding/contracts/CrowdFunding.sol
@@ -36,7 +36,20 @@ contract CrowdFunding {
         return numberOfCampaigns - 1;
     }
 
-    function donateToCampaign() {}
+    function donateToCampaign(uint256 _id) public payable {
+        uint256 amount = msg.value;
+
+        Campaign storage campaign = campaigns[_id];
+
+        campaign.donators.push(msg.sender);
+        campaign.donations.push(amount);
+
+        (bool sent,) = payable(campaign.owner).call{value: amount}("");
+
+        if(sent) {
+            campaign.amountCollected = campaign.amountCollected + amount;
+        }
+    }
 
     function getDonators() {}
 
