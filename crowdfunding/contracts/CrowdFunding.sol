35e0ac4ba6cbeab91bf90b1de3de352f93f3695f
Dexter49 <dexterousguru49@yahoo.com>
2023-11-08 14:39:23 +0100

    Finished the last function, which is the getCampaigns()` function... With that, I'm done with building the smart contract for Fundr

--- crowdfunding/contracts/CrowdFunding.sol
+++ crowdfunding/contracts/CrowdFunding.sol
@@ -55,7 +55,16 @@ contract CrowdFunding {
         return (campaigns[_id].donators, campaigns[_id].donations);
     }
 
-    function getCampaigns() {}
+    function getCampaigns() public view returns (Campaign[] memory) {
+        Campaign[] memory allCampaigns = new Campaign[](numberOfCampaigns);
 
+        for(uint i = 0; i < numberOfCampaigns; i++) {
+            Campaign storage item = campaigns[i];
+
+            allCampaigns[i] = item;
+        }
+
+        return allCampaigns;
+    }
 
 }
